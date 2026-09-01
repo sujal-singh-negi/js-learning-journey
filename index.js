8beb75a61f33dbd534efc6ce0ef069e49fc29db3
sujal-singh-negi <sujalsinghnegi123@gmail.com>
2026-09-01 20:28:55 +0530

    Day 1: Variables and Type Conversion Practice

---
+++ index.js
@@ -0,0 +1,37 @@
+console.log("Hello World");
+const accountId = 144553;
+let accountEmail = "sujal@google.com";
+var accountCity = "Agra";
+const state = "UP";
+
+accountEmail = "negi@google.com";
+accountCity = "Delhi";
+console.table([accountId, accountEmail, accountCity, state]);
+
+let score = 33;
+console.log(typeof score);
+
+let scoreInString = "33";
+let valueInNumber = Number(scoreInString);
+
+
+console.log(typeof valueInNumber);
+
+let myAge = "20";
+let myAgeAsNumber = Number(myAge);
+console.log(typeof myAgeAsNumber);
+
+let myScore = "100abc";
+let convertedScore = Number(myScore);
+console.log(typeof convertedScore);
+console.log(convertedScore);
+
+let myScoreline = "33abc";
+let myScorelineAsNumber = Number(myScoreline);
+
+console.log(typeof myScorelineAsNumber)
+console.log(myScorelineAsNumber)
+
+let isLoggedIn = 1;
+let BooleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(BooleanIsLoggedIn);
