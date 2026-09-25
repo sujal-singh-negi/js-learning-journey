ed6af40ffa834d17066e2fdd7fd45332645ae345
sujal-singh-negi <sujalsinghnegi123@gmail.com>
2026-09-25 18:09:46 +0530

    Feat: completed this control flow

--- index.js
+++ index.js
@@ -759,21 +759,64 @@ const temperature = 41
 // }
 
 const month = "march"
-switch (month) {
-    case "jan":
-        console.log("Janaury");
-        break;
-    case "feb":
-        console.log("February");
-        break;
-    case "march":
-        console.log("March");
-        break;
+// switch (month) {
+//     case "jan":
+//         console.log("Janaury");
+//         break;
+//     case "feb":
+//         console.log("February");
+//         break;
+//     case "march":
+//         console.log("March");
+//         break;
 
-    default:
-        console.log("Default case match");
-        break;
-}
+//     default:
+        // console.log("Default case match");
+        // break;
+// }
+
+// const userEmail = "s@sujal.com"
+
+// if (userEmail) {
+//     console.log("Got User email");
+    
+// } else {
+//     console.log("Dont have user email");
+    
+// }
+
+// falsy values 
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN++++++++++++++++
+// truthy values 
+// "0", 'false', " ", [], {}, function(){}
+
+// const userEmail = []
+// if (userEmail.length === 0) {
+//     console.log("Array is Empty");
+    
+// }
+
+// const emptyObj = {}
+// if (Object.keys(emptyObj.length === 0)) {
+//     console.log("Object is empty");
+    
+// }
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// console.log(val1);
+
+// Terniary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+// iceTeaPrice  <= 80 ? console.log("less than 80") : console.log("more than 80");
 
 
 
